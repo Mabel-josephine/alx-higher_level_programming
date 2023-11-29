0b6de514115d12e0325149659d3ac9118e684d81
Mabel-josephine <kuukuabruce55@gmail.com>
2023-11-29 11:48:42 -0800

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for num in range(99):
-    print('{0} = {1}'.format(num, hex(num)))
-    if i % 5 == 4:
-        print()
+    print(f'{num} {num:x}") = {1}'.format(num, hex(num)))

4c11ec05b53eb75f987f3d62e4cd2c8d2f286659
Mabel-josephine <kuukuabruce55@gmail.com>
2023-11-29 10:13:30 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-ascii_value_a = ord('a')
-for i in range(20, 40):
-    if chr(ascii_value_a + i) not in ['q', 'e']:
-        continue
-    print(chr(i).format(i), end='')
+name = "Mabel"
+age = 20
+height = 1.65
+
+print(f"My name is {name}, I am {age} years old, and my height {height} meters.")
+

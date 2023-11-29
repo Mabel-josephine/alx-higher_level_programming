13409d5684893b64a9edfb2ae22b798db3cd9dfa
Mabel-josephine <kuukuabruce55@gmail.com>
2023-11-29 10:46:40 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 for char in map(chr, range(ord('a'), ord('z') + 1)):
-    print(f"{char}", end="")
-    if char != 'z':
-        print("",end="")
+    print(f"{char}", end="" if char != 'z' else "\n")

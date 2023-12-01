9a7c3564c5285373e5c9a94cf1135d4db510b299
Mabel-josephine <kuukuabruce55@gmail.com>
2023-11-30 19:09:38 -0800

    0-add.py

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: 1
+        b: 2
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+

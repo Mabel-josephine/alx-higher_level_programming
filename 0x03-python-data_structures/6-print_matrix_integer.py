4d611eae661450936a00a262af1d5ba9e000cdb2
Mabel-josephine <kuukuabruce55@gmail.com>
2023-12-08 04:13:14 -0800

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0 or len(matrix[0]) == 0:
+        return None
+
+    large = matrix[0][0]
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end=" ")
-        print()
+            if large < element:
+                large = element
+
+    return large
